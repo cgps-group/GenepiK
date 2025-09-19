c331e05834cc873cda2aa858e85f3b2571add062
varunshamanna <varunshamanna4@gmail.com>
2025-09-19 17:55:44 +0100

    updated description to include all the packages

--- R/create_ast_barplots_gene.R
+++ R/create_ast_barplots_gene.R
@@ -30,7 +30,7 @@ create_ast_barplots_gene <- function(masterdata, output_dir) {
   )]
 
   # 2. Create resistance indicator
-  AST_data$carba_resistance<-ifelse(AST_data$Bla_Carb_acquired == "-"|is.na(AST_data$carba_resistance),
+  AST_data$carba_resistance<-ifelse(AST_data$Bla_Carb_acquired == "-"|is.na(AST_data$Bla_Carb_acquired),
                                     "CARBA-S","CARBA-R")
 
   # 3. Pivot from wide to long format
